5631b6e530771edeee65f052084ff6c033d64554
Carlos Andres Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-03-13 15:31:35 -0500

    Modifica Multiplicacion para tener sólo 2 parámetros

--- Multiplicacion.java
+++ Multiplicacion.java
@@ -1,5 +1,5 @@
 public class Multiplicacion {
-    public void multiplicar (int numero1, int numero2, int numero3){
-        System.out.println(numero1 * numero2 * numero3);
+    public void multiplicar (float numero1, float numero2){
+        System.out.println(numero1 * numero2);
     }
 }
